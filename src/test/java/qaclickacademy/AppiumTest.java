6ebe08477efcc46e1cff09b192fe12a74cb1642a
A0707400 <shubham.chibu@gmail.com>
2019-11-22 21:23:41 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void navtiveAppAndriod()
+
+    {
+        System.out.println("navtiveAppAndriod");
+    }
+    @Test
+    public void iosApps()
+    {
+        System.out.println("iosApps");
+    }
+}
---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+    @Test
+    public void postJira()
+
+    {
+        System.out.println("postJira");
+    }
+    @Test
+    public void deleteTewitter()
+    {
+        System.out.println("deleteTwitter");
+    }
+
+
+}
